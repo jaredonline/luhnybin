01643c08f56de82ae4e449e6098e05b587b7e3c1
Jared McFarland <jared.online@gmail.com>
2011-12-05 02:35:33 -0800

    a lot faster with more optimization

--- src/looney.c
+++ src/looney.c
@@ -7,11 +7,18 @@
 #define MINCARDLENGTH 14
 #define MAXCARDLENGTH 16
 
-int char_count;
 char input [MAXBUFFERSIZE];
 char output [MAXBUFFERSIZE];
 char ch;
 
+/*
+  the pan_tracker type keeps all the information for a single possible PAN:
+    start: the index of input that this possible PAN starts at
+    type: the type of PAN (14, 15 or 16-digit PAN)
+    count: how many digits this pan_tracker has accumulated
+    total: the current running total according to the Luhn check
+    skip: already ID'ed as a PAN (or not a PAN) so skip it in all actions
+*/
 typedef struct {
   int start;
   int type;
@@ -21,7 +28,7 @@ typedef struct {
 } pan_tracker;
 
 pan_tracker pans [MAXBUFFERSIZE * 3];
-int pan_tracker_count;
+int pan_tracker_count, char_count;
 
 int main(int argc, char* argv[])
 {    
@@ -29,49 +36,63 @@ int main(int argc, char* argv[])
   
   while( (ch = getchar()) != EOF && char_count <= MAXBUFFERSIZE) {
     
+    /*
+      reset the state on a new line:
+       - (set a NULL character for output) and print the output string
+       - set all PAN trackers to skip
+       - reset counters
+    */
     if (ch == '\n') {
       output[char_count] = 0x00;
-      int i;
-      for (i = 0; i < char_count; i++) putchar(output[i]);
-      printf("\n");
+      printf("%s\n", output);
       fflush(stdout);
       
+      int i;
       for (i = 0; i < pan_tracker_count; i++) {
-        pan_tracker pan = {0, 0, 0, 1, 1};
-        pans[i] = pan;
+        pans[i].skip = 1;
       }
       
       char_count = pan_tracker_count = 0;
       continue;
     }  
     
+    // add new character to the buffers
     input[char_count] = output[char_count] = ch;
     
+    // setup the new pan_trackers; one for each type
     int type;
     for (type = MINCARDLENGTH; type <= MAXCARDLENGTH; type++) {
       pan_tracker pan = {char_count, type, 0, 0, 0};
       pans[pan_tracker_count++] = pan;
     }
     
+    /*
+      update the state of each PAN tracker and mask the
+      output if a PAN is detected.  If a PAN tracker goes
+      over it's designated length, set it to skip (it's no
+      longer a possible PAN).
+    */
     int x = ch - '0';
     int i;
     for (i = 0; i < pan_tracker_count; i++) {
-      if (x >= 0 && x <= 9) {
-        pans[i].total += ((pans[i].count % 2) == (pans[i].type == 15 ? 1 : 0)) ? ((x * 2) % 10) + ((x * 2) / 10): x;
-        pans[i].count++;
-      }
-      
-      if (pans[i].count == pans[i].type && pans[i].total % 10 == 0 && pans[i].skip == 0) {
-        int a = pans[i].start;
-        int x_count = 0;
-        while (x_count < pans[i].type) {
-          if (input[a] >= '0' && input[a] <= '9') {
-            output[a] = 'X';
-            x_count++;
-          } 
-          a++;
+      if (x >= 0 && x <= 9 && pans[i].skip == 0) {
+        pans[i].total += ((pans[i].count++ % 2) == (pans[i].type == 15 ? 1 : 0)) ? ((x * 2) % 10) + ((x * 2) / 10): x;
+        
+        if (pans[i].count == pans[i].type && pans[i].total % 10 == 0) {
+          int a = pans[i].start;
+          int x_count = 0;
+          while (x_count < pans[i].type) {
+            if (input[a] >= '0' && input[a] <= '9') {
+              output[a] = 'X';
+              x_count++;
+            } 
+            a++;
+            pans[i].skip = 1;
+          }
+        } else if (pans[i].count > pans[i].type) {
           pans[i].skip = 1;
         }
+        
       }
     }
     
